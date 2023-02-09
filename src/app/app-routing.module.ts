8e0625792221e5b07e2ecc743bbdc03ee6f6af46
purushothamreddyaccionlabs <purushotham.reddy@accionlabs.com>
2023-02-09 09:50:44 +0530

    9/2/2023

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent,canActivate:[AuthGuard]},
+  {path:'statistics',component:StatisticsComponent, canActivate:[AuthGuard]},
+  {path:'',component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
